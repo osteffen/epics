22de4ff0ead7f846fe58cd2aba60584605ac318a
Oliver Steffen <steffen@kph.uni-mainz.de>
2014-05-26 19:00:47 +0200

    Nicer ezca example

--- examples/ezca/simple_get/main.c
+++ examples/ezca/simple_get/main.c
@@ -8,9 +8,9 @@
 int main(int argc, char **argv) {
 
     // allocate memory for the values
-	double faradaycup = 0;
-	double ionchamber = 0;	// P2
-	double livetime = 0;
+    double* d = (double*)malloc(argc*sizeof(double));
+
+    int i;
 
     // EPICS: this has to be done at the start of the program
     ezcaAutoErrorMessageOff();
@@ -21,21 +21,23 @@ int main(int argc, char **argv) {
         //        by doing this as a "group" it is fast
         ezcaStartGroup();
 
-        	ezcaGet("BEAM:FaradayCup", ezcaDouble, 1, &faradaycup);
-        	ezcaGet("BEAM:IonChamber", ezcaDouble, 1, &ionchamber);
-        	ezcaGet("TRIG:TotalLivetime", ezcaDouble, 1, &livetime);
+            // read all PVs we want
+            for( i=1;i<argc;++i)
+                ezcaGet(argv[i], ezcaDouble, 1, &d[i]);
 
         // end the block read and check for errors
         if (ezcaEndGroup())
             ezcaPerror(NULL);
 
         // print the result
-        printf("Faraday Cup    = %f\n",faradaycup);
-        printf("IonChamber(P2) = %f\n",ionchamber);
-        printf("Total Livetime = %f\n",livetime);
+        for( i=1;i<argc;++i)
+            printf("%s\t%f\n",argv[i],d[i]);
 
         sleep(1); // wait 1 sec
 
     }
+
+    // free memory again
+    free((void*)d);
 
 } /* end main() */
