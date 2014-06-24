1917ff8665d7c045bf25f0221bb63a54e76ed22f
Oliver Steffen <steffen@kph.uni-mainz.de>
2014-06-24 21:23:10 +0200

    Gateway config

--- iocs/gateway/start.sh
+++ iocs/gateway/start.sh
@@ -2,14 +2,23 @@
 
 . /opt/epics/thisEPICS.sh
 
-#IP to collent pv data from. This is the hall side
-SOURCE_IP=10.32.168.174
+#IP to send relayed data off
+SERVER_IP=slowcontrol.office.a2.kph
 
 #Do not read pv data back from our own IP on the hall side
-SOURCE_IP_IGNORE=10.32.161.39
+SOURCE_IP_IGNORE=slowcontrol.online.a2.kph
 
-#Output dv data on this IP in the office net
+# Online network broadcast address
 CLIENT_IP=10.32.167.255
 
-exec $EPICS_EXTENSIONS/bin/$EPICS_HOST_ARCH/gateway -sip $SOURCE_IP -signore $SOURCE_IP_IGNORE -cip $CLIENT_IP
+echo "Starting EPICS Gateway."
+# echo "See /var/log/epics_gateway/log for logs"
+exec $EPICS_EXTENSIONS/bin/$EPICS_HOST_ARCH/gateway \
+  -signore $SOURCE_IP_IGNORE \
+  -sip $SERVER_IP \
+  -cip $CLIENT_IP \
+  -home /var/run/epics_gateway
 
+# logging is disabled for now.
+# messages go to the screen terminal
+#  -log /var/log/epics_gateway/log \
